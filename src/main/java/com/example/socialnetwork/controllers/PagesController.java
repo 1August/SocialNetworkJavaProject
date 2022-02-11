0f1b400d8ad38e26c27a7f8aaf0a26b957d0a4de
Demidog03 <83386838+Demidog03@users.noreply.github.com>
2022-02-11 20:28:49 +0600

    Front Commit2

--- src/main/java/com/example/socialnetwork/controllers/PagesController.java
+++ src/main/java/com/example/socialnetwork/controllers/PagesController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PagesController {
     @GetMapping("/")
     String indexGet(){
-        return "index";
+        return "userProfile";
     }
 }
