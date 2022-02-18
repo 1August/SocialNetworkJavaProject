132372a0870d7813f20eafd81fe850b2f679c186
Maksat <boktaban@gmail.com>
2022-02-18 12:35:20 +0600

    Tried, but nothing happened...(So sad ,_,)

--- src/main/java/com/example/socialnetwork/WebSocket/WebSocketConfig.java
+++ src/main/java/com/example/socialnetwork/WebSocket/WebSocketConfig.java
@@ -1,5 +1,3 @@
-package com.example.socialnetwork.WebSocket;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
@@ -15,21 +13,21 @@ import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class  WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker( "/user"); // For sending and receiving messages
-//        config.setApplicationDestinationPrefixes("/app");  // Prefix for methods @MessageMapping
+        config.enableSimpleBroker( "/user");
+        config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
-
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/user/post");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ourWebsocket").withSockJS();
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
@@ -43,3 +41,52 @@ public class  WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         return false;
     }
 }
+
+
+
+
+//package com.example.socialnetwork.WebSocket;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.converter.DefaultContentTypeResolver;
+//import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+//import org.springframework.messaging.converter.MessageConverter;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.util.MimeTypeUtils;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class  WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker( "/user"); // For sending and receiving messages
+////        config.setApplicationDestinationPrefixes("/app");  // Prefix for methods @MessageMapping
+//        config.setUserDestinationPrefix("/user");
+//
+//        config.enableSimpleBroker("/topic");
+//        config.setApplicationDestinationPrefixes("/user/post");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+//    }
+//
+//    @Override
+//    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+//        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+//        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+//        converter.setObjectMapper(new ObjectMapper());
+//        converter.setContentTypeResolver(resolver);
+//        messageConverters.add(converter);
+//        return false;
+//    }
+//}
--- src/main/java/com/example/socialnetwork/controllers/MessageController.java
+++ src/main/java/com/example/socialnetwork/controllers/MessageController.java
@@ -1,20 +1,14 @@
 package com.example.socialnetwork.controllers;
-import com.example.socialnetwork.entities.Post;
-import com.example.socialnetwork.models.ResponsePost;
 
+import com.example.socialnetwork.entities.Post;
 import com.example.socialnetwork.services.PostService;
 import com.example.socialnetwork.services.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MessageController {
@@ -25,17 +19,64 @@ public class MessageController {
     @Autowired
     PostService postService;
 
-
-    @MessageMapping("/user/post")
-    @SendTo("/topic/user/posts")
-    public ResponsePost getPost(String obj) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(obj, ResponsePost.class);
-    }
-
-    @MessageMapping("/post")
+    @MessageMapping("/chat")
     void processPost(@Payload Post post){
         userService.getUserById(post.getAuthor().getId());
         postService.savePost(post);
     }
+
+
+    @GetMapping("/ws")
+    String websocketGet(){
+        return "WebSocketConnection";
+    }
 }
+
+
+//package com.example.socialnetwork.controllers;
+//import com.example.socialnetwork.entities.Post;
+//import com.example.socialnetwork.models.ResponsePost;
+//
+//import com.example.socialnetwork.services.PostService;
+//import com.example.socialnetwork.services.UserService;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.util.HtmlUtils;
+//
+//
+//@Controller
+//public class MessageController {
+//
+//    @Autowired
+//    UserService userService;
+//
+//    @Autowired
+//    PostService postService;
+//
+//
+//    @MessageMapping("/user/post")
+//    @SendTo("/topic/user/posts")
+//    public ResponsePost getPost(String obj) throws JsonProcessingException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        return objectMapper.readValue(obj, ResponsePost.class);
+//    }
+//
+//    @MessageMapping("/post")
+//    void processPost(@Payload Post post){
+//        userService.getUserById(post.getAuthor().getId());
+//        postService.savePost(post);
+//    }
+//
+//    @GetMapping("/websocket")
+//    String websocketGet(){
+//        return "WebSocketConnection";
+//    }
+//}
